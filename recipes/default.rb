f433bb34f35a327bfbe9cd44392a80857fc0af0f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-09 11:34:37 +0100

    create a node recipe

---
+++ recipes/default.rb
@@ -0,0 +1,26 @@
+package "make"
+package "g++"
+
+git "/opt/nodejs" do
+  repository "git://github.com/joyent/node.git"
+  revision "v0.10.25"
+  action :sync
+end
+
+execute "configure nodejs" do
+  cwd "/opt/nodejs"
+  command "./configure"
+  creates "/opt/nodejs/config.mk"
+end
+
+execute "make nodejs" do
+  cwd "/opt/nodejs"
+  command "make"
+  creates "/opt/nodejs/out"
+end
+
+execute "make install nodejs" do
+  cwd "/opt/nodejs"
+  command "make install"
+  creates "/usr/local/bin/node"
+end
